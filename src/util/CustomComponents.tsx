0adf01334c6ae024bdd6ac2e0d4246fb85ed569e
Protowalker <jacksonedwards6@gmail.com>
2021-10-30 18:03:43 -0400

    fix typographyinput by adding whiteSpace: pre

--- src/util/CustomComponents.tsx
+++ src/util/CustomComponents.tsx
@@ -1,15 +1,13 @@
-import { Input, InputProps, Text, TypographyProps } from "@chakra-ui/react";
+import { Input, InputProps, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
-export const TypographyInput = (
-  props: InputProps & { fontSize: TypographyProps["fontSize"] }
-) => {
+export const TypographyInput = (props: InputProps) => {
   const [widthRef, setWidthRef] = useState<HTMLElement | null>(null);
   const [width, setWidth] = useState("10ch");
   useEffect(() => {
     setWidth(
       widthRef?.getBoundingClientRect().width.toString() + "px" ??
-        (props.value as string)?.length + "ch" ??
+        (props.value as string)?.length.toString() + "ch" ??
         "20ch"
     );
   }, [widthRef, props.value]);
@@ -17,27 +15,16 @@ export const TypographyInput = (
   return (
     <>
       <Text
+        {...props}
         component="span"
         ref={(ref: HTMLElement | null) => setWidthRef(ref)}
-        fontSize={props.fontSize}
-        sx={{
-          ...props.sx,
-          position: "absolute",
-          visibility: "hidden",
-        }}
+        position="absolute"
+        visibility="hidden"
+        whiteSpace="pre"
       >
         {props.value as string}
       </Text>
-      <Input
-        {...props}
-        fontSize={props.fontSize}
-        variant="unstyled"
-        sx={{
-          ...props.sx,
-          p: 0,
-          width: width,
-        }}
-      />
+      <Input {...props} variant="unstyled" p={0} width={width} />
     </>
   );
 };
