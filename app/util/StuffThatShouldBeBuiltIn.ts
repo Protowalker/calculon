28ce3c90f5144391aad747df1691edf85bc013f6
Jackie Edwards <jackieedwards@milestechnologies.com>
2022-09-30 15:31:47 -0400

    add assert util function

--- app/util/StuffThatShouldBeBuiltIn.ts
+++ app/util/StuffThatShouldBeBuiltIn.ts
@@ -1,1 +1,8 @@
 export const waitFor = (ms: number) => new Promise((resolve, _) => setTimeout(resolve, ms));
+
+export function assert<T>(cond: T, message?: string): asserts cond {
+    if(cond) {
+        return;
+    } 
+    throw new Error("assertion failed: " + message);
+}
